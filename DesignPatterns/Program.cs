525eefd0acc3076cd71c8e47afb5840878f46f56
danieldanwii <daniel.wiinberg@norconsult.com>
2024-07-09 11:09:10 +0200

    Open closed

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+
+namespace DesignPatterns
+{
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // SINGLE RESPONSIBILITY
+            #region
+            //var j = new SingleResponsibility.Journal();
+            //j.AddEntry("Suck one");
+            //j.AddEntry("Suck two");
+            //Console.WriteLine((j));
+            #endregion
+
+            // OPEN-CLOSED PRINCIPLE
+            #region
+            var apple = new Product("Apple", Color.Red, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+            var hat = new Product("Hat", Color.Green, Size.Small);
+
+            Product[] products = { apple, tree, house, hat };
+            var productFilter = new ProductFilter();
+
+            WriteLine("Green products (old):");
+            foreach (var product in productFilter.FilterByColor(products, Color.Green))
+            {
+                WriteLine($"{product.Name} is {product.Color}");
+            }
+
+            var betterFilter = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach(var product in betterFilter.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                WriteLine($"{product.Name} is {product.Color}");
+            }
+
+            WriteLine("Large green items");
+            foreach (var product in betterFilter.Filter(products, 
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Green), 
+                    new SizeSpecification(Size.Large)
+                    )))
+            {
+                WriteLine($"{product.Name} is {product.Size} and {product.Color}");
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ DesignPatterns/SOLID Principles/OpenClosedPrinciple.cs
@@ -0,0 +1,136 @@
+﻿
+namespace DesignPatterns
+{
+    // OPEN-CLOSED PRINCIPLE
+    // Classes should be open for extension but closed for modification (shouldn't be necessary to modify the actual class)
+    // It shouldn't be necessary to make changes to classes that may already have been shipped and deployed to a customer. 
+    // Instead classes should be made in such a way that they can easily be extended by modules that can be shipped later. 
+
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("name");
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color)
+                {
+                    yield return product;
+                }
+            }
+        }
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Color color, Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color && product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+    }
+
+    public class BetterFilter: IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> products, ISpecification<Product> specification)
+        {
+            foreach(Product product in products)
+            {
+                if(specification.IsSatisfied(product))
+                    yield return product;
+            }
+        }
+    }
+
+    public interface ISpecification<T> //Enterprise pattern
+    {   
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color) 
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product product) 
+        { 
+            return product.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+}
